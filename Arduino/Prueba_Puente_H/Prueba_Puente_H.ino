e0a670f19c14e45071f72fbd85681ca95d80cefe
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2022-05-26 01:32:14 -0500

    La Programacion de Arduino esta lista solo toca conectarla al Serial y listo, voy a factorizar el codigo del lector

--- Arduino/Prueba_Puente_H/Prueba_Puente_H.ino
+++ Arduino/Prueba_Puente_H/Prueba_Puente_H.ino
@@ -21,8 +21,12 @@ int izqB = 6;
 int derA = 9; 
 int derB = 10; 
 int vel = 255; // Velocidad de los motores (0-255)
+int led_S= 13; // Pines de conexion para led's
+int led_N= 15; // Pin de conexion led
+int salida;
 
 void setup()  { 
+  Serial.begin(9600);
   pinMode(derA, OUTPUT);
   pinMode(derB, OUTPUT);
   pinMode(izqA, OUTPUT);
@@ -30,25 +34,70 @@ void setup()  {
  } 
  
 void loop()  { 
+  /*
+    Serial.println("Detenido");
     analogWrite(derB, 0);  // Detiene los Motores
     analogWrite(izqB, 0); 
     delay (500);
+    
+    Serial.println("Frente");
     analogWrite(derA, vel);  // Frente 2 segundos
     analogWrite(izqA, vel); 
     delay (2000);
-   
+    
+    Serial.println("Derecha");
     analogWrite(derA, vel);  // Derecha 0,5 segundos
     analogWrite(izqA, 0); 
     delay (500);
-    
+
+    Serial.print("Izquierda")
     analogWrite(derA, 0);    // Izquierda 0,5 segundos
     analogWrite(izqA, vel); 
     delay (500);
-     
+    
+    Serial.println("Detenido");
     analogWrite(derA, 0);    // Detiene los Motores
     analogWrite(izqA, 0);
     delay (500);
+
+    Serial.println("Reversa");
     analogWrite(derB, vel);  // Reversa 2 segundos
     analogWrite(izqB, vel); 
     delay (2000);                      
+    */
+    if(Serial.available()>0){
+        salida=Serial.read();
+        Serial.println(salida);
+        if(salida=='S'){
+          digitalWrite(led_S, HIGH);
+
+          Serial.println("Derecha");
+          analogWrite(derA, vel);   // Los mueve a la derecha
+          delay(5000);
+
+          Serial.println("Detenido");
+          analogWrite(derA, 0);    // Detiene los Motores
+          delay(2000);
+
+          /*Serial.println("Reversa");
+          analogWrite(derB, vel);  // Reversa 2 segundos
+          // analogWrite(izqB, vel);
+          delay(5000);
+
+          Serial.println("Detenido");
+          analogWrite(derA, 0);    // Detiene los Motores
+          analogWrite(izqA, 0);
+          */
+                   
+        }
+        if(salida=='N'){
+          digitalWrite(led_N, HIGH);
+          delay(2000);
+
+          Serial.println("Detenido");
+          analogWrite(derA, 0);    // Detiene los Motores
+          analogWrite(izqA, 0);
+        }
+      
+    }
 }
